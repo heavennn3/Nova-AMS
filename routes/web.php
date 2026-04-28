1536bd5bee3f3ef91686475ba93db264a413bcfe
Muhammad Safuan <muhammadsafuan5756@gmail.com>
2026-04-28 15:45:36 +0800

    first

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Features;
+
+Route::inertia('/', 'welcome', [
+    'canRegister' => Features::enabled(Features::registration()),
+])->name('home');
+
+
+Route::inertia('/dashboard', 'dashboard', [
+   
+])->name('dashboard');
+
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::inertia('dashboard', 'dashboard')->name('dashboard');
+});
+
+require __DIR__.'/settings.php';
