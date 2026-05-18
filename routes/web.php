be92154818008e3d7c236b2766dcde162aa5b654
MUHAMMAD SAFUAN <muhammadsafuan5756@gmail.com>
2026-05-18 16:29:21 +0800

    style: add blank line after imports in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
+
 Route::inertia('/', 'nova-ams', [
     'canRegister' => Features::enabled(Features::registration()),
 ])->name('Nova AMS');
