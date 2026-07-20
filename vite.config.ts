0bf70f736e1ff87dced25141047393bfd034cfe0
MUHAMMAD SAFUAN <muhammadsafuan5756@gmail.com>
2026-07-20 14:49:24 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,16 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: '192.168.1.68',
+            protocol: 'ws',
+        },
+    },
+
+
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
